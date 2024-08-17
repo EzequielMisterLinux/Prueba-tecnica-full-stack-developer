3ccb3ab7c30ad0fed2a162510cd79c18ed2dd6a4
EzequielMisterLinux <humbertoezequiel.z.c@gmail.com>
2024-08-16 23:14:03 -0600

    fix a tasks controller correccion de importaciones

--- backend/src/tasks/tasks.controller.ts
+++ backend/src/tasks/tasks.controller.ts
@@ -1,4 +1,28 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Post, Put, Delete, Body, Param } from '@nestjs/common';
+import { TasksService } from './tasks.service';
+import { Task } from './models/task.model';
 
 @Controller('tasks')
-export class TasksController {}
+export class TasksController {
+  constructor(private readonly tasksService: TasksService) {}
+
+  @Get()
+  async findAll(): Promise<Task[]> {
+    return this.tasksService.findAll();
+  }
+
+  @Post()
+  async create(@Body() task: Partial<Task>): Promise<Task> {
+    return this.tasksService.create(task);
+  }
+
+  @Put(':id')
+  async update(@Param('id') id: string, @Body() task: Partial<Task>): Promise<Task> {
+    return this.tasksService.update(id, task);
+  }
+
+  @Delete(':id')
+  async delete(@Param('id') id: string): Promise<Task> {
+    return this.tasksService.delete(id);
+  }
+}
