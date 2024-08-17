8956aaba8c7ce7d87d82dfb5ed49cb464cd35298
EzequielMisterLinux <humbertoezequiel.z.c@gmail.com>
2024-08-16 23:04:44 -0600

    agregando aplicacion web a corregir ciertos problemas

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import macrosPlugin from "vite-plugin-babel-macros"
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react({
+      babel: {
+        plugins: ['babel-plugin-macros', 'babel-plugin-styled-components'],
+      },
+    }),
+    macrosPlugin(),
+  ],
+  define: {
+    'process.env': {}
+  }
 })
